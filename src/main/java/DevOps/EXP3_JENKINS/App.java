69cf32c0da9f897562a1b5ee0eed6e1e85eab545
LazyPunk <lazypunk@pop-os.localdomain>
2023-10-25 11:30:45 +0530

    updated

--- src/main/java/DevOps/EXP3_JENKINS/App.java
+++ src/main/java/DevOps/EXP3_JENKINS/App.java
@@ -4,7 +4,14 @@ public class App {
     public static void greeting() {
         System.out.println("Hello World!");
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         greeting();
+        int result = add(3, 5);
+        System.out.println("The sum of 3 and 5 is: " + result);
     }
 }
